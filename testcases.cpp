d05def193e9ad068d69afd1f4cb7b32ee4d345b9
Chi-Yen Chen <chiyench@usc.edu>
2019-04-06 20:16:38 -0700

    Update testcases

--- testcases.cpp
+++ testcases.cpp
@@ -8,6 +8,9 @@
 #include "catch.hpp"
 #include "LRUcache.hpp"
 
+/** Testcase
+ *  Getting elements from empty LRU cache
+ */ 
 TEST_CASE("get element from empty LRU cache", ) {
     LRUcache cache(3);
     REQUIRE(cache.get(1)==false);
@@ -19,8 +22,10 @@ TEST_CASE("get element from empty LRU cache", ) {
     REQUIRE(cache.get(3)==true);
     REQUIRE(cache.get(4)==true);
 }
-
-TEST_CASE("general test for LRU cache") {
+/** Testcase
+ *  General usage of LRU cache
+ */ 
+TEST_CASE("general test1") {
     LRUcache cache(2);
     cache.put(1);
     cache.put(2);
@@ -31,17 +36,20 @@ TEST_CASE("general test for LRU cache") {
     REQUIRE(cache.get(3)==true);
     REQUIRE(cache.get(4)==false);
 }
-TEST_CASE("general test") {
+/** Testcase
+ *  General usage of LRU cache,
+ *  with negative numbers as key and get not existing elements
+ */ 
+TEST_CASE("general test with negative numbers") {
     LRUcache cache(3);
     cache.put(1);
-    cache.put(2);
+    cache.put(-2);
     REQUIRE(cache.get(1)==true);
     cache.put(3);
-    REQUIRE(cache.get(2)==true);
+    REQUIRE(cache.get(-2)==true);
     cache.put(4);
     REQUIRE(cache.get(1)==false);
-    REQUIRE(cache.get(2)==true);
+    REQUIRE(cache.get(2)==false);
     REQUIRE(cache.get(3)==true);
     REQUIRE(cache.get(4)==true);
 }
-// 4, 2, 3
